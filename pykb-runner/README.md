c53595730013ca489f6cd22947d2fe27ebb7035c
michael mccune <msm@opbstudios.com>
2024-01-17 21:58:46 -0500

    update pykb runner to use config file

--- pykb-runner/README.md
+++ pykb-runner/README.md
@@ -3,3 +3,18 @@
 This directory contains artifacts to create a kube-burner runner that
 will run a series of tests in serial fashion, reading configuration
 data from a configmap.
+
+## configuration data
+
+The configuration file for pykb-runner is `/etc/pykb-runner/config.yaml`
+and it has the following format:
+
+```yaml
+runs:
+  - iterations: 1
+    churn-duration: 1h
+    churn-delay: 20m
+  - iterations: 4
+    churn-duration: 30m
+    churn-delay: 10m
+```
--- pykb-runner/entrypoint.py
+++ pykb-runner/entrypoint.py
@@ -1,14 +1,38 @@
 #!/bin/env python3
 import logging
 import subprocess
+import sys
+import yaml
 
 
 def main():
     logging.basicConfig(level=logging.INFO)
     logging.info("Starting kube-burner runner")
 
-    subprocess.run(["kube-burner", "ocp", "cluster-scaling", "--help"])
+    try:
+        raw = open("/etc/pykb-runner/config.yaml").read()
+        data = yaml.load(raw, Loader=yaml.Loader)
+    except Exception as ex:
+        logging.error("unable to read configuration data")
+        raise ex
+    logging.info("loaded configuration")
 
+    for i, r in enumerate(data.get("runs", [])):
+        iterations = r.get("iterations")
+        churn_duration = r.get("churn-duration")
+        churn_delay = r.get("churn-delay")
+
+        if not iterations or not churn_duration or not churn_delay:
+            logging.error(f"run {i} misconfigured, check configuration data")
+            sys.exit(1)
+
+        kb_args = [f"--iterations={iterations}",
+                   f"--churn-duration={churn_duration}",
+                   f"--churn-delay={churn_delay}",
+                   ]
+        kb_cmd = ["kube-burner", "ocp", "cluster-scaling"]
+        logging.info(f"Running command {kb_cmd + kb_args}")
+        subprocess.run(kb_cmd + kb_args)
 
 
 if __name__ == "__main__":
