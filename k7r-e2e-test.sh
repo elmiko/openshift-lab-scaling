408ac33890257511038ae05f59c46abb68c73d85
Julio Faerman <356476+faermanj@users.noreply.github.com>
2024-01-31 18:40:35 +0000

    wip

--- k7r-e2e-test.sh
+++ k7r-e2e-test.sh
@@ -3,11 +3,13 @@ set -x
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 export ENV_ID="k7r$RANDOM"
-echo $ENV_ID > .env-id.txt
+export TEST_CASE="$1"
 
 export CLUSTER_NAME="$ENV_ID"
 export INSTALL_DIR="${DIR}/.install-dir/$CLUSTER_NAME"
 
+echo "Running e2e test [$TEST_CASE] on [$ENV_ID]"
+sleep 5
 # check if openshift-insall is not available
 if ! command -v openshift-install &> /dev/null
 then
@@ -21,10 +23,17 @@ source ./create-cluster.aws-e2e.sh
 echo "Checking cluster is accessible"
 oc cluster-info
 
-TEST_CASE=$1
+
 if [ -n "$TEST_CASE" ]; then
     echo "Running test case $TEST_CASE"
     # Code for the case where the variable is set
+    TEST_SCRIPT="./k7r-e2e-$TEST_CASE.sh"
+    if [ -x "$TEST_SCRIPT" ]; then
+        echo "$TEST_SCRIPT found, executing..."
+        source $TEST_SCRIPT
+    else
+        echo "$TEST_SCRIPT not found"
+    fi
 else
     echo "No test case passed, running no test case"
     # Code for the case where the variable is not set or is empty
