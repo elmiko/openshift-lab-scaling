07f49e7567037298dc0cfe9c875a98bea134a8bc
Julio Faerman <356476+faermanj@users.noreply.github.com>
2024-02-05 18:23:16 +0000

    wip

--- destroy-cluster.e2e.sh
+++ destroy-cluster.e2e.sh
@@ -2,9 +2,5 @@
 set -x
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-echo "Destroying cluster [$ENV_ID]"
-
-export CLUSTER_NAME="$ENV_ID"
-export INSTALL_DIR="${DIR}/.install-dir/$CLUSTER_NAME"
-
-openshift-install destroy cluster --dir=$INSTALL_DIR --log-level=debug
+echo "Destroying cluster [$INSTALL_DIR]"
+openshift-install destroy cluster --dir="$INSTALL_DIR" --log-level=debug
--- k7r-e2e-prune.sh
+++ k7r-e2e-prune.sh
@@ -3,6 +3,9 @@
 TAG_NAME="cluster_name"
 TAG_VALE="$CLUSTER_NAME"
 
+echo "## Done pruning cluster [$TAG_NAME=$TAG_VALUE]" 
+
+echo "## Terminating instances" 
 # Terminate EC2 instances
 aws ec2 describe-instances --filters "Name=tag:$TAG_NAME,Values=$TAG_VALUE" --query 'Reservations[*].Instances[*].InstanceId' --output text | while read instanceId
 do
@@ -11,6 +14,7 @@ do
 done
 
 # Delete Load Balancers
+echo "## Deleting load balancers" 
 aws elbv2 describe-load-balancers --query 'LoadBalancers[*].LoadBalancerArn' --output text | while read loadBalancerArn
 do
     echo "Deleting Load Balancer: $loadBalancerArn"
@@ -18,6 +22,7 @@ do
 done
 
 # Delete Subnets
+echo "## Deleting subnets" 
 aws ec2 describe-subnets --filters "Name=tag:$TAG_NAME,Values=$TAG_VALUE" --query 'Subnets[*].SubnetId' --output text | while read subnetId
 do
     echo "Deleting Subnet: $subnetId"
@@ -25,6 +30,7 @@ do
 done
 
 # Delete Security Groups
+echo "## Describe security groups" 
 aws ec2 describe-security-groups --filters "Name=tag:$TAG_NAME,Values=$TAG_VALUE" --query 'SecurityGroups[*].GroupId' --output text | while read groupId
 do
     echo "Deleting Security Group: $groupId"
@@ -32,10 +38,11 @@ do
 done
 
 # Delete VPCs
+echo "## Delete VPCs" 
 aws ec2 describe-vpcs --filters "Name=tag:$TAG_NAME,Values=$TAG_VALUE" --query 'Vpcs[*].VpcId' --output text | while read vpcId
 do
     echo "Deleting VPC: $vpcId"
     aws ec2 delete-vpc --vpc-id $vpcId
 done
 
-echo "Done pruning cluster [$TAG_NAME=$TAG_VALUE]"
+echo "## Done pruning cluster [$TAG_NAME=$TAG_VALUE]" 
